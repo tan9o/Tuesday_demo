f9d9ca35d7ba1c5e86e32741ada0262de938b8ba
Amol <includechintu@gmail.com>
2020-11-17 13:37:27 +0530

    All done

--- calc.py
+++ calc.py
@@ -1,8 +1,15 @@
 def add(x,y):
 	return x+y
 
+#<<<<<<< Updated upstream
 def sub (x,y):
 	return x-y
 
 def multiply(x,y):
 	return x*y
+#=======
+
+
+def pow (x,y):
+	return x**y
+#>>>>>>> Stashed changes
